fea111c4ea409e8aaafc039fc543dcb173cf6f67
shivamsingh0510 <144701993+shivamsingh0510@users.noreply.github.com>
2023-10-22 04:45:18 +0000

    contaner config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "sde@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId= 2 //not allowed to change the cont value
+
+accountEmail = "abd@gmail.com"
+accountPassword = "233445"
+accountCity ="delhi" 
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity,accountId])
