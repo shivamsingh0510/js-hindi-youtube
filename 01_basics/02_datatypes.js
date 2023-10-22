159cafbc5c299dce436e4bfbf59b0cfd40cb088b
shivamsingh0510 <144701993+shivamsingh0510@users.noreply.github.com>
2023-10-22 18:01:44 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js as newer version
+// alert(3+3)//we ar4e using node js , not browser
+
+let name = "shivam"
+let age = 18
+let loggedIn = false
+let state;
+
+
+//number => 2^53
+//bignit
+//string => ""
+//boolean =>true/false
+//null =>stnadalone value
+//undefined => 
+//symbool =>unique
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
