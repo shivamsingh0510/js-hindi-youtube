badb0de1d421359f5c122369892145e7582d1f65
shivamsingh0510 <144701993+shivamsingh0510@users.noreply.github.com>
2023-10-23 03:15:32 +0000

    datatypes summary

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -21,3 +21,46 @@ let state;
 console.log(typeof age);
 console.log(typeof null);//object
 console.log(typeof undefined);//unde
+
+
+//Datatypes summary
+//primitive => 7 categories => call by value.
+//7 types: string , Number ,Boolean, null,undefined,symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+// const id = symbool('123')
+// const anotherId = symbool('123')
+// console.log(id ===anotherId);
+const bigNumber = 223467456787654345678765n
+ 
+
+//refrence datatype (non primitive)
+
+//array , object, functions 
+
+const heros = ["shaktiman","naagraj","doga"];
+
+let myObj = {
+    name:"shivam",
+    age :21,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+//to find the data type
+console.log(typeof bigNumber);
+console.log(typeof temperature);
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+ 
