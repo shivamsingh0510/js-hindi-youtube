cc99d9528b7e7832fe4e721c32cec6d94f9eb48e
shivamsingh0510 <144701993+shivamsingh0510@users.noreply.github.com>
2023-10-23 03:16:30 +0000

    conversionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -22,7 +22,26 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 //*********************operations ***********************/
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+
+let str1 = "shivam"
+let str2 = "singh"
+
+let str3 = str1 +str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+
