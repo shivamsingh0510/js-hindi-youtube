01a7da617fc605610d3cb697f93a378442121984
shivamsingh0510 <144701993+shivamsingh0510@users.noreply.github.com>
2023-10-30 13:17:33 +0000

    array_basics_01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,61 @@
+//array
+
+const myArr = [0,1,2,3,4,5];
+// const myHeroes = ["papa"]
+//array of js is resizable i.e we can add or delete element in array
+
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[3]);//by indexing
+
+//array method
+// myArr.push(6)
+
+// myArr.push(7)
+//push method is used to add the element in the last of the array.
+// myArr.pop()//Here pop method is used to remove the last value in the array
+
+//  myArr.unshift(9);// here unshift shift the element of whole array and put the insert value in the front of the array
+
+//  but there is the problem to use unshift
+
+// myArr.shift(9)
+// shift method remove the first element from the array.
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// if the index value in - it denotes the value not exist in the array
+
+// const newArr = myArr.join()
+// Here join method is use to display the element and change in to string.
+
+
+// console.log(myArr);//Here typeof---
+// console.log(typeof newArr);//Here typeof----string
+
+// Slice and Splice+++++++++++++++++++++++++++++++++++
+
+// console.log("A",myArr);
+
+// const myn1 = myArr.slice(1,3)
+
+
+
+// console.log(myn1);
+// console.log("B",myArr);
+
+// const myn2 = myArr.splice(1,3)
+
+// When we apply splice then manipulation in the original array as we can clearly see in the C ouput.
+
+// console.log("c ",myArr);
+
+// console.log(myn2);
+
+
+
+
+
+
+
+
+
+
